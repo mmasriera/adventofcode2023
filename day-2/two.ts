9511330e4c4cb9c8b7b34b2ed4750dff3285425a
Miquel Masriera <miquelmasriera@gmail.com>
2023-12-08 01:13:38 +0100

    day2 part 2 (#4)

---
+++ day-2/two.ts
@@ -0,0 +1,39 @@
+
+import { readInputLines } from '../utils/index.ts';
+
+type Color = 'red' | 'blue' | 'green';
+
+const getValue = (subset: string): [number, Color] => {
+	const [amount, color] = subset.split(' ');
+
+	return [parseInt(amount, 10), color as Color];
+}
+
+const cubesSetPower = (game: string): number => {
+	const cubes = { red: 0, green: 0, blue: 0 };
+
+	for (const round of game.split('; ')) {
+		for (const subset of round.split(', ')) {
+			const [amount, color] = getValue(subset);
+
+			if (cubes[color] < amount) {
+				cubes[color] = amount;
+			}
+		}
+	}
+
+	return Object.values(cubes).reduce((acc, curr) => acc * curr, 1);
+}
+
+const lines = await readInputLines('./input.txt');
+
+let result = 0;
+
+for (const line of lines) {
+	const game = line.split(': ')[1];
+
+	result += cubesSetPower(game);
+}
+
+console.log(`-> answer: ${ result }`);
+// solution 67363
