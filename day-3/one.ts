c414c7fb2289a92466e51af941bb8b0060840838
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-08 02:57:05 +0100

    day3 1

--- day-3/one.ts
+++ day-3/one.ts
@@ -1,10 +1,30 @@
 
 import { readInputLines } from '../utils/index.ts';
 
+const NUMBERS_REGEX = /\d+/g;
+const SYMBOL_REGEX = /[^(\.|\d)]/;
+
+const hasAdjacent = (lineIdx: number, start: number, end: number): boolean => {
+	const prev = lines[lineIdx - 1]?.substring(start, end) || '';
+	const curr = lines[lineIdx]?.substring(start, end) || '';
+	const next = lines[lineIdx + 1]?.substring(start, end) || '';
+	
+	return SYMBOL_REGEX.test(prev + curr + next); // check in all surrounding chars
+};
+
 const lines = await readInputLines('./input.txt');
+let result = 0;
 
-for (const line of lines) {
-	// ...
-}
+lines.forEach((line, lineIdx) => {
+	line.match(NUMBERS_REGEX)?.forEach(num => {
+		// for each number
+		const startIdx = line.indexOf(num);
+		const endIdx = startIdx + num.length;
 
-console.log(`-> answer: ${ result }`); // x
+		if (hasAdjacent(lineIdx, startIdx - 1, endIdx + 1)) { // +-1 to get diagonals
+			result += Number(num);
+		}
+	});
+});
+
+console.log(`-> answer: ${ result }`); // 535294 is too high
