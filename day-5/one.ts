a72e8fe9b6f8ad383656ca2750a75c50fd0e5cc9
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-12 02:26:43 +0100

    day part 1

--- day-5/one.ts
+++ day-5/one.ts
@@ -1,34 +1,64 @@
 
 import { readInputLines } from '../utils/index.ts';
 
-const lines = await readInputLines('./inputTEST.txt');
+const lines = await readInputLines('./input.txt');
 
-const seeds = lines[0].split('seeds: ')[1].split(' ');
-const maps: Record<string, unknown> = {
-	'seed-to-soil map:': {},
-	'soil-to-fertilizer map:': {},
-	'fertilizer-to-water map:': {},
-	'water-to-light map:': {},
-	'light-to-temperature map:': {},
-	'temperature-to-humidity map:': {},
-	'humidity-to-location map:': {}
+const seeds = lines[0].split('seeds: ')[1].split(' ').map(Number);
+
+type Range = {
+	src: number;
+	dest: number;
+	range: number;
+};
+const maps: Record<string, Range[]> = {
+	'seed-to-soil map:': [],
+	'soil-to-fertilizer map:': [],
+	'fertilizer-to-water map:': [],
+	'water-to-light map:': [],
+	'light-to-temperature map:': [],
+	'temperature-to-humidity map:': [],
+	'humidity-to-location map:': []
 };
 
+// init map
 const NUMBERS_REGEX = /\d+/g;
+let currentMap;
 for (let i = 1; i < lines.length; i += 1) {
 	if (maps[lines[i]]) {
-
-		do {
-			i += 1;
-
-			const [destStart, srcStart, range] = lines[i].match(NUMBERS_REGEX);
-
-
-		} while( destStart );
+		currentMap = maps[lines[i]];
+	} else {
+		const [dest, src, range] = (lines[i].match(NUMBERS_REGEX) || []).map(Number);
 		
+		if (dest || src || range) {
+			currentMap!.push({ dest, src, range });
+		}
 	}
 }
 
-let result = 0;
+const getDestination = (key: string, source: number): number => {
+	for (let i = 0; i < maps[key].length; i += 1) {
+		const { src, dest, range } = maps[key][i];
 
-console.log(`-> answer: ${ result }`); // x
+		if ((source >= src) && (source <= (src + range))) { // if is in range
+			return dest + (source - src);
+		}
+	}
+
+	return source;
+}
+
+const seedToSoil = (seed: number): number => {	
+	const soil = getDestination('seed-to-soil map:', seed);
+	const fertilizer = getDestination('soil-to-fertilizer map:', soil);
+	const water = getDestination('fertilizer-to-water map:', fertilizer);
+	const light = getDestination('water-to-light map:', water);
+	const temperature = getDestination('light-to-temperature map:', light);
+	const humidity = getDestination('temperature-to-humidity map:', temperature);
+	const location = getDestination('humidity-to-location map:', humidity);
+
+	return location;
+}
+
+const result = Math.min(...seeds.map(seedToSoil));
+
+console.log(`-> answer: ${ result }`); // 389056265
