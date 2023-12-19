da198bd19c4867bebc08b248a9669b644cf5f1a5
Miquel Masriera <miquelmasriera@gmail.com>
2023-12-19 23:21:01 +0100

    Day5 2 (#9)
    * day 5
    
    * day part 1
    
    * day5 part 2
    
    * rething part one
    
    * part 2
    
    * part2

--- day-5/two.ts
+++ day-5/two.ts
@@ -1,12 +1,41 @@
 
-import { readInputLines } from '../utils/index.ts';
+import { readInput } from '../utils/index.ts';
 
-const lines = await readInputLines('./input.txt');
+const input = await readInput('./input.txt');
+const [seedsLine, ...mappingsLine] = input.split('\n\n');
+const seeds = seedsLine.split('seeds: ')[1].split(' ').map(Number);
+const seedRanges = [];
+for (let i = 0; i < seeds.length; i += 2) {
+	seedRanges.push([seeds[i], seeds[i + 1]]);
+}
+const mappings = mappingsLine.map(line => 
+	line.split('\n').slice(1) // split by endlines and remove the 1st one "x to y:"
+	.map(numbers => numbers.split(' ').map(Number)) // get the numbers
+);
 
-for (const line of lines) {
-	// ...
+const getLocation = (seed: number): number => {
+	let value = seed;
+	
+	for (const mapping of mappings) {
+		for (const [dest, src, range] of mapping) { // iterate over all the mappings to get the dest
+			if ((value >= src) && (value <= (src + range))) { // if is in range
+				value = dest + (value - src);
+				break;
+			}
+		}
+	}
+
+	return value;
 }
 
-let result = 0;
+const result = Math.min(...seedRanges.map(([start, size]) => {
+	let min = Number.MAX_SAFE_INTEGER;
 
-console.log(`-> answer: ${ result }`); // x
+	for (let i = start; i < start + size; i += 1) {
+		min = Math.min(min, getLocation(i));
+	}
+
+	return min;
+}));
+
+console.log(`-> answer: ${ result }`); //
