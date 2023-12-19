614a9c5c8d3b76dfd9e3e337df5e535fb12df1d5
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-19 23:39:15 +0100

    day3 1

--- day-3/one.ts
+++ day-3/one.ts
@@ -15,16 +15,34 @@ const hasAdjacent = (lineIdx: number, start: number, end: number): boolean => {
 const lines = await readInputLines('./inputTEST.txt');
 let result = 0;
 
-lines.forEach((line, lineIdx) => {
-	line.match(NUMBERS_REGEX)?.forEach(num => {
-		// for each number
-		const startIdx = line.indexOf(num);
-		const endIdx = startIdx + num.length;
+// for (let i = 0; i < lines.length; i += 1) {
+// 	console.log('=>', lines[i]);
 
-		if (hasAdjacent(lineIdx, startIdx - 1, endIdx + 1)) { // +-1 to get diagonals
-			result += Number(num);
-		}
+// 	const numbers = lines[i].replace(/\./g, " ");
+
+//     for (const match of numbers.matchAll(/\d+/g)) {
+// }
+
+lines.forEach((line, lineIdx) => {
+	const lineSpaced = line.replace(/\./g, ' ');
+	
+	Array.from(lineSpaced.matchAll(NUMBERS_REGEX)).forEach(match => {
+		// const result = match[0]
+		// console.log(match[0], match.index);
+		console.log(match[0], match.index);
+		
+		
 	});
+
+// 	line.match(NUMBERS_REGEX)?.forEach(num => {
+// 		// for each number
+// 		const startIdx = line.indexOf(num);
+// 		const endIdx = startIdx + num.length;
+
+// 		if (hasAdjacent(lineIdx, startIdx - 1, endIdx + 1)) { // +-1 to get diagonals
+// 			result += Number(num);
+// 		}
+// 	});
 });
 
 console.log(`-> answer: ${ result }`); // 535294
