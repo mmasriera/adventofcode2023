6c643be2b094f98b31dd886610a44b101e169953
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-26 00:50:35 +0100

    input

---
+++ day-7/two.ts
@@ -0,0 +1,12 @@
+
+import { readInputLines } from '../utils/index.ts';
+
+const lines = await readInputLines('./input.txt');
+
+for (const line of lines) {
+	// ...
+}
+
+let result = 0;
+
+console.log(`-> answer: ${ result }`); // x
