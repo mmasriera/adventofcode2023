e7431cd7c1bb4be557dd6fcdfe0c1eb30e6f96b9
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-25 23:33:36 +0100

    3

--- day-3/one.ts
+++ day-3/one.ts
@@ -4,45 +4,30 @@ import { readInputLines } from '../utils/index.ts';
 const NUMBERS_REGEX = /\d+/g;
 const SYMBOL_REGEX = /[^.\d]/;
 
-const hasAdjacent = (lineIdx: number, start: number, end: number): boolean => {
-	const prev = lines[lineIdx - 1]?.substring(start, end) || '';
-	const curr = lines[lineIdx].substring(start, end) || '';
-	const next = lines[lineIdx + 1]?.substring(start, end) || '';
-	
-	return SYMBOL_REGEX.test(prev + curr + next); // check in all surrounding chars
-};
-
-const lines = await readInputLines('./inputTEST.txt');
+const lines = await readInputLines('./input.txt');
 let result = 0;
 
-// for (let i = 0; i < lines.length; i += 1) {
-// 	console.log('=>', lines[i]);
+lines.forEach((line: string, i: number) => {	
+	Array.from(line.matchAll(NUMBERS_REGEX)).forEach(match => {		
+		for (let j = match.index; j < match.index + match[0].length; j += 1) {
+			const surrounding = [
+				(lines[i - 1] ?? '')[j - 1] ?? '.',
+				(lines[i - 1] ?? '')[j] ?? '.',
+				(lines[i - 1] ?? '')[j + 1] ?? '.',
+				(lines[i] ?? '')[j - 1] ?? '.',
+				(lines[i] ?? '')[j] ?? '.',
+				(lines[i] ?? '')[j + 1] ?? '.',
+				(lines[i + 1] ?? '')[j - 1] ?? '.',
+				(lines[i + 1] ?? '')[j] ?? '.',
+				(lines[i + 1] ?? '')[j + 1] ?? '.'
+			];
 
-// 	const numbers = lines[i].replace(/\./g, " ");
-
-//     for (const match of numbers.matchAll(/\d+/g)) {
-// }
-
-lines.forEach((line, lineIdx) => {
-	const lineSpaced = line.replace(/\./g, ' ');
-	
-	Array.from(lineSpaced.matchAll(NUMBERS_REGEX)).forEach(match => {
-		// const result = match[0]
-		// console.log(match[0], match.index);
-		console.log(match[0], match.index);
-		
-		
+			if (surrounding.some(x => SYMBOL_REGEX.test(x))) {
+				result += parseInt(match[0]);
+				break;
+			}
+        }
 	});
-
-// 	line.match(NUMBERS_REGEX)?.forEach(num => {
-// 		// for each number
-// 		const startIdx = line.indexOf(num);
-// 		const endIdx = startIdx + num.length;
-
-// 		if (hasAdjacent(lineIdx, startIdx - 1, endIdx + 1)) { // +-1 to get diagonals
-// 			result += Number(num);
-// 		}
-// 	});
 });
 
-console.log(`-> answer: ${ result }`); // 535294
+console.log(`-> answer: ${ result }`); // 535078
