cd7c84721e27bcfb43c0fcb6c3be646d2c70ae70
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-26 00:37:44 +0100

    day 6 part 2

--- day-6/two.ts
+++ day-6/two.ts
@@ -1,12 +1,35 @@
 
 import { readInputLines } from '../utils/index.ts';
 
+const NUMBERS_REGEX = /\d+/g;
+
 const lines = await readInputLines('./input.txt');
 
-for (const line of lines) {
-	// ...
+const time = Number(lines[0].match(NUMBERS_REGEX)!.join(''));
+const dist = Number(lines[1].match(NUMBERS_REGEX)!.join(''));
+
+// start holdTime
+let start = 1;
+for (let holdTime = 1; holdTime < time; holdTime += 1) {
+	const distance = holdTime * (time - holdTime);
+
+	if (distance > dist) {
+		start = holdTime;
+		break;
+
+	}
 }
 
-let result = 0;
+// end holdTime
+let end = 1;
+for (let holdTime = time; time > start; holdTime -= 1) {
+	const distance = holdTime * (time - holdTime);
 
-console.log(`-> answer: ${ result }`); // x
+	if (distance > dist) {
+		end = holdTime;
+		break;
+
+	}
+}
+
+console.log(`-> answer: ${ end - start + 1 }`); // 24655068
