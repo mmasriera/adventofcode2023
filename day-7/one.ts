4a2077419b3c0fe54c5f77db558906aabb0bd451
miquel masriera quevedo <miquelmasriera@gmail.com>
2023-12-31 01:25:19 +0100

    day7 part1

--- day-7/one.ts
+++ day-7/one.ts
@@ -1,10 +1,14 @@
 
 import { readInputLines } from '../utils/index.ts';
 
+const RANK = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 const lines = await readInputLines('./input.txt');
 
 for (const line of lines) {
-	// ...
+	const [hand, bid] = line.split(' ');
+
+	console.log(hand);
 }
 
 let result = 0;
